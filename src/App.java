beaac20e93e9a8ef3d835c341e8dae5a045fc0bd
GabrielleAzevedo20 <gabypedroso20@gmail.com>
2022-02-02 12:11:43 -0300

    Learning Java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Night City!");
-        System.out.println("Judy Alvarez!");
     }
 }
